5d80307b96acf88692f5b61b38a194779793ee2d
Cactiw <Knyazev1514@gmail.com>
2024-05-21 01:53:04 +0300

    Add recreate script

--- recreate.sh
+++ recreate.sh
@@ -1,4 +1,4 @@
-VBoxManage list vms |cut -d "{" -f2 |cut -d "}" -f1 |xargs -L1 VBoxManage unregistervm
+VBoxManage list vms |grep inaccessible |cut -d "{" -f2 |cut -d "}" -f1 |xargs -L1 VBoxManage unregistervm
 vagrant destroy -f
 docker context rm 2022-box || true
 vagrant up --provider virtualbox 2022-box
